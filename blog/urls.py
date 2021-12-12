f524f3b2adbed3ffdf1527b4060ce07e80741e9d
dotpy88 <tsmith.py@gmail.com>
2021-12-12 08:24:38 -0600

    blog static fixed

--- blog/urls.py
+++ blog/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 
+
 from . import views
 
 app_name = 'blog'
 urlpatterns = [
         path('', views.home, name='home'),
         #re_path(r'^Torrents/ListView$', Torrents.torrent_list, name='torrentslist'),
-]
+] 
--- mdjblog/settings.py
+++ mdjblog/settings.py
@@ -118,5 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
