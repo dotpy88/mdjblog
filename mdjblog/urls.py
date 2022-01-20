d23ce8100584fbd00958c67ba3939f0a511f6ee1
dotpy88 <tsmith.py@gmail.com>
2022-01-20 05:31:40 -0600

    rearranged urls

--- mdjblog/urls.py
+++ mdjblog/urls.py
@@ -19,6 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
